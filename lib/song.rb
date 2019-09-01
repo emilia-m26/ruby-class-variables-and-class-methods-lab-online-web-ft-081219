dcb957b4adbe0a458356d74c2710625aa8fe0366
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-01 17:35:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 class Song 
   attr_accessor :name, :artist, :genre
   @@count=0 
-  
+  @@artists=[]
+  @@genres=[]
   
   def initialize(name, artist, genre)
     @name=name 
@@ -17,11 +18,11 @@ class Song
   end
   
   def self.genres 
-    @@genres=[]
+    @@genres.uniq
   end
   
   def self.artists 
-    @@artists=[]
+    @@artists.uniq
   end 
   
   def self.genre_count
@@ -33,62 +34,3 @@ class Song
   
   
 end
-
-
-class Song 
-  attr_accessor :name, :artist, :genre
-  
-  @@count = 0
-  @@genres = []
-  @@artists=[]
-  
-  def initialize(name, artist, genre)
-    @name=name
-    @artist=artist
-    @genre=genre
-    @@count += 1
-    @@genres << genre
-    @@artists << artist
-  end
-  
-  def self.count
-    @@count
-  end
-  
-  def self.genres
-    @@genres.uniq
-  end 
-  
-    def self.artists
-      @@artists.uniq
-    end
-    
-    def self.genre_count
-      genre_count={}
-      @@genres.each do |genre|
-        if genre_count[genre]
-          genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    return genre_count
-  end
-  
-  def self.artist_count
-    artist_count={}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1 
-    end
-  end
-  return artist_count
-end
-
-end
-
-
-
-
